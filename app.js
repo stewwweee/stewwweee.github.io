a40572bebbd9e96f97f5aa5a3bbe05432ea6309a
stewwweee <31616719+stewwweee@users.noreply.github.com>
2023-03-04 00:34:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-// app.js
-
-const passport = require('passport');
-const GitHubStrategy = require('passport-github2').Strategy;
-
-passport.use(new GitHubStrategy({
-  clientID: 3a0e3b7c59e031ab742c,
-  clientSecret: dcd69f1d0f8fafd20d22216a4f8b8157fef24882,
-  callbackURL: "https://chrisstewartblog.netlify.app/"
-},
-function(accessToken, refreshToken, profile, done) {
-  User.findOrCreate({ githubId: profile.id }, function (err, user) {
-    return done(err, user);
-  });
-}
-));
-
-app.get('/auth/github',
-  passport.authenticate('github', { scope: [ 'user:email' ] }));
-
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+/* Hide the header when scrolling down */
+var lastScroll = 0;
+$(window).on('scroll', function() {
+  var currentScroll = $(this).scrollTop();
+  if (currentScroll > lastScroll) {
+    $('.site-header').addClass('site-header--hidden');
+  } else {
+    $('.site-header').removeClass('site-header--hidden');
+  }
+  lastScroll = currentScroll;
+});
